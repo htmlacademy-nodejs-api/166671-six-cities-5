8460108d96a41f3e75c514e048ad824ac19f7ae9
vpalex999 <vpalex@mail.ru>
2023-09-27 09:08:07 +0500

    add TSVFileReader

---
+++ src/shared/libs/file-reader/tsv-file-reader.ts
@@ -0,0 +1,70 @@
+import { readFileSync } from 'node:fs';
+import { FileReader } from './file-reader.interface.js';
+import { RentalOffer } from '../../../types/rental-offer.interface.js';
+import { TypeHousing } from '../../../types/type-housing.type.js';
+import {
+  adaptBooleanFromString,
+  adaptCoordFromString,
+  adaptHousingFeaturesFromString,
+  adaptPhotosHousingFromString,
+} from '../../adapters/fromImportFile.js';
+
+export class TSVFileReader implements FileReader {
+  private rawData = '';
+
+  constructor(private readonly filename: string) {}
+
+  public read(): void {
+    this.rawData = readFileSync(this.filename, { encoding: 'utf-8' });
+  }
+
+  public toArray(): RentalOffer[] {
+    if (!this.rawData) {
+      throw new Error('File was not read');
+    }
+
+    return this.rawData
+      .split('\n')
+      .filter((row) => row.trim().length > 0)
+      .map((line) => line.split('\t'))
+      .map(
+        ([
+          title,
+          authorId,
+          city,
+          coords,
+          datePublication,
+          description,
+          features,
+          isFavorites,
+          isPremium,
+          numberComments,
+          numberGuests,
+          numberRooms,
+          photosHousing,
+          previewImage,
+          rating,
+          rentalPrice,
+          typeHousing,
+        ]) => ({
+          authorId,
+          city,
+          coords: adaptCoordFromString(coords),
+          datePublication: new Date(datePublication),
+          description,
+          features: adaptHousingFeaturesFromString(features),
+          isFavorites: adaptBooleanFromString(isFavorites),
+          isPremium: adaptBooleanFromString(isPremium),
+          numberComments: +numberComments,
+          numberGuests: +numberGuests,
+          numberRooms: +numberRooms,
+          photosHousing: adaptPhotosHousingFromString(photosHousing),
+          previewImage,
+          rating: +rating,
+          rentalPrice: +rentalPrice,
+          title,
+          typeHousing: typeHousing as TypeHousing,
+        })
+      );
+  }
+}
