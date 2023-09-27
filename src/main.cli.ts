5918c34bc1cc9ba711e46b1ccdf1f5aa3455e5a7
vpalex999 <vpalex@mail.ru>
2023-09-27 09:30:54 +0500

    register ImportCommand

--- src/main.cli.ts
+++ src/main.cli.ts
@@ -1,9 +1,13 @@
 import { CLIApplication } from './cli/cli-application.js';
-import { HelpCommand, VersionCommand } from './cli/index.js';
+import { HelpCommand, ImportCommand, VersionCommand } from './cli/index.js';
 
 const bootstrap = () => {
   const cliApplication = new CLIApplication();
-  cliApplication.registerCommand([new HelpCommand(), new VersionCommand()]);
+  cliApplication.registerCommand([
+    new HelpCommand(),
+    new VersionCommand(),
+    new ImportCommand(),
+  ]);
 
   cliApplication.processCommand(process.argv);
 };
