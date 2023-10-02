f4957e434beca7901383f93283b7d999bea6a52d
vpalex999 <vpalex@mail.ru>
2023-10-02 08:13:53 +0500

    add type MockServerData

---
+++ src/types/mock-server-data.type.ts
@@ -0,0 +1,18 @@
+export type MockServerData = {
+  authorsId: string[];
+  cities: string[];
+  datePublications: string[];
+  descriptions: string[];
+  features: string[];
+  isFavorites: [boolean, boolean];
+  isPremium: [boolean, boolean];
+  numberComments: number[];
+  numberGuests: number[];
+  numberRooms: number[];
+  photosHousing: string[];
+  previewImages: string[];
+  ratings: number[];
+  rentalPrices: number[];
+  titles: string[];
+  typeHousing: string[];
+};
