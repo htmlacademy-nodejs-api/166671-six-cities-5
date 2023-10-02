ac585233f6be93b5c837f155edff5e9f8a3e472f
vpalex999 <vpalex@mail.ru>
2023-10-02 18:22:46 +0500

    add get mock data from server

--- src/cli/commands/generate.command.ts
+++ src/cli/commands/generate.command.ts
@@ -1,12 +1,34 @@
+import got from 'got';
+import { MockServerData } from '../../types/mock-server-data.type.js';
 import { Command } from './command.interface.js';
 
 export class GenerateCommand implements Command {
+  private initialData: MockServerData;
+
+  private async load(url: string) {
+    try {
+      this.initialData = await got.get(url).json();
+    } catch {
+      throw new Error(`Can't load data from ${url}`);
+    }
+  }
+
   public getName(): string {
     return '--generate';
   }
 
-  public execute(...parameters: string[]): void {
+  public async execute(...parameters: string[]): Promise<void> {
     const [count, filepath, url] = parameters;
     const offerCount = Number.parseInt(count, 10);
+
+    try {
+      await this.load(url);
+    } catch (error: unknown) {
+      console.error('Can\'t  generate data');
+
+      if (error instanceof Error) {
+        console.error(error.message);
+      }
+    }
   }
 }
