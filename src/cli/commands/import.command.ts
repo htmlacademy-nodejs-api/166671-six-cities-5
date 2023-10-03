b3051291dee8939277027d700b3d9c4fe2942187
vpalex999 <vpalex@mail.ru>
2023-10-03 19:12:12 +0500

    add read from stream to ImportCommand

--- src/cli/commands/import.command.ts
+++ src/cli/commands/import.command.ts
@@ -1,26 +1,35 @@
 import chalk from 'chalk';
 import { TSVFileReader } from '../../shared/libs/file-reader/tsv-file-reader.js';
 import { Command } from './command.interface.js';
+import { createOffer } from '../../shared/helpers/offer.js';
+import { getErrorMessage } from '../../shared/helpers/common.js';
 
 export class ImportCommand implements Command {
   public getName(): string {
     return '--import';
   }
 
-  public execute(...parameters: string[]): void {
+  private onImportedLine(line: string) {
+    const offer = createOffer(line);
+    console.info(offer);
+  }
+
+  private onCompleteImport(count: number) {
+    console.info(`${count} rows imported`);
+  }
+
+  public async execute(...parameters: string[]): Promise<void> {
     const [filename] = parameters;
     const fileReader = new TSVFileReader(filename.trim());
 
-    try {
-      fileReader.read();
-      console.log(fileReader.toArray());
-    } catch (error) {
-      if (!(error instanceof Error)) {
-        throw error;
-      }
+    fileReader.on('line', this.onImportedLine);
+    fileReader.on('end', this.onCompleteImport);
 
-      console.log(chalk.red(`Can't import data from file: ${filename}`));
-      console.log(chalk.red(`Details: ${error.message}`));
+    try {
+      await fileReader.read();
+    } catch (error) {
+      console.error(chalk.red(`Can't import data from file: ${filename}`));
+      console.error(getErrorMessage(error));
     }
   }
 }
