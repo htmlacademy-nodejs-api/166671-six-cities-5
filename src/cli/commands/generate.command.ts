9c8c3a350ce5f0057e2538360ed76dd69a01b076
vpalex999 <vpalex@mail.ru>
2023-10-03 19:28:04 +0500

    add write on stream to GenerateCommand

--- src/cli/commands/generate.command.ts
+++ src/cli/commands/generate.command.ts
@@ -2,7 +2,8 @@ import got from 'got';
 import { MockServerData } from '../../types/mock-server-data.type.js';
 import { Command } from './command.interface.js';
 import { TSVOfferGenerator } from '../../shared/libs/offer-generator/tsv-offer-generator.js';
-import { appendFile } from 'node:fs/promises';
+import { TSVFileWriter } from '../../shared/libs/file-writer/tsv-file-writer.js';
+import { getErrorMessage } from '../../shared/helpers/common.js';
 
 export class GenerateCommand implements Command {
   private initialData: MockServerData;
@@ -17,10 +18,10 @@ export class GenerateCommand implements Command {
 
   private async write(filepath: string, offerCount: number) {
     const tsvOfferGenerator = new TSVOfferGenerator(this.initialData);
+    const tsvFileWriter = new TSVFileWriter(filepath);
+
     for (let i = 0; i < offerCount; i++) {
-      await appendFile(filepath, `${tsvOfferGenerator.generate()}\n`, {
-        encoding: 'utf-8',
-      });
+      await tsvFileWriter.write(tsvOfferGenerator.generate());
     }
   }
 
@@ -37,11 +38,8 @@ export class GenerateCommand implements Command {
       await this.write(filepath, offerCount);
       console.log(`File ${filepath} was created!`);
     } catch (error: unknown) {
-      console.error('Can\'t  generate data');
-
-      if (error instanceof Error) {
-        console.error(error.message);
-      }
+      console.error('Can not generate data');
+      console.error(getErrorMessage(error));
     }
   }
 }
