a611077d014dd9d0fa9b877fc48cac7ffad6bcb0
vpalex999 <vpalex@mail.ru>
2023-10-07 07:20:04 +0500

    add config to restApplication

--- src/main.rest.ts
+++ src/main.rest.ts
@@ -1,10 +1,12 @@
 import { PinoLogger } from './shared/libs/logger/pino.logger.js';
 import { RestApplication } from './rest/rest.application.js';
+import { RestConfig } from './shared/libs/config/rest.config.js';
 
 const bootstrap = async () => {
   const logger = new PinoLogger();
+  const config = new RestConfig(logger);
 
-  const application = new RestApplication(logger);
+  const application = new RestApplication(logger, config);
   await application.init();
 };
 
