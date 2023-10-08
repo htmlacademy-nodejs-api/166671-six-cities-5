c0be01593d27af5d5e0c9b99da30ae767bc87029
vpalex999 <vpalex@mail.ru>
2023-10-08 10:45:16 +0500

    add container DI to main.rest

--- src/main.rest.ts
+++ src/main.rest.ts
@@ -1,12 +1,20 @@
-import { PinoLogger } from './shared/libs/logger/pino.logger.js';
-import { RestApplication } from './rest/rest.application.js';
+import { Container } from 'inversify';
+import { RestApplication } from './rest/index.js';
+import { Component } from './shared/types/component.enum.js';
+import { Logger, PinoLogger } from './shared/libs/logger/index.js';
 import { RestConfig } from './shared/libs/config/rest.config.js';
+import { RestSchema } from './shared/libs/config/rest.schema.js';
+import { Config } from './shared/libs/config/index.js';
 
 const bootstrap = async () => {
-  const logger = new PinoLogger();
-  const config = new RestConfig(logger);
+  const container = new Container();
+  container
+    .bind<RestApplication>(Component.RestApplication)
+    .to(RestApplication);
+  container.bind<Logger>(Component.Logger).to(PinoLogger);
+  container.bind<Config<RestSchema>>(Component.Config).to(RestConfig);
 
-  const application = new RestApplication(logger, config);
+  const application = container.get<RestApplication>(Component.RestApplication);
   await application.init();
 };
 
