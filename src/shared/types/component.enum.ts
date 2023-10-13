18fed4f561757266864f794c09554e733b432bfe
vpalex999 <vpalex@mail.ru>
2023-10-13 18:23:35 +0500

    add DatabaseClient to Component

--- src/shared/types/component.enum.ts
+++ src/shared/types/component.enum.ts
@@ -2,4 +2,5 @@ export const Component = {
   RestApplication: Symbol.for('RestApplication'),
   Logger: Symbol.for('Logger'),
   Config: Symbol.for('Config'),
+  DataBaseClient: Symbol.for('DatabaseClient'),
 } as const;
