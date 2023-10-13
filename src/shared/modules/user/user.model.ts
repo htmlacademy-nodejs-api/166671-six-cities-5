f323c97244eeb4cb270dd9b5044e1deab872611a
vpalex999 <vpalex@mail.ru>
2023-10-14 04:28:27 +0500

    expand the interface UserDocument

--- src/shared/modules/user/user.model.ts
+++ src/shared/modules/user/user.model.ts
@@ -1,14 +1,20 @@
 import { Document, Schema, model } from 'mongoose';
 import { User } from '../../types/index.js';
 
-export interface UserDocument extends User, Document {}
+export interface UserDocument extends User, Document {
+  createdAt: Date;
+  updatedAt: Date;
+}
 
-const userSchema = new Schema({
-  avatar: String,
-  email: String,
-  name: String,
-  password: String,
-  userType: String,
-});
+const userSchema = new Schema(
+  {
+    avatar: String,
+    email: String,
+    name: String,
+    password: String,
+    userType: String,
+  },
+  { timestamps: true }
+);
 
 export const UserModel = model<UserDocument>('User', userSchema);
