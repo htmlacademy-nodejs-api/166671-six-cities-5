f443c09185f0ce3b5938a6d4ade5d60735a340a1
vpalex999 <vpalex@mail.ru>
2023-10-14 08:16:06 +0500

    add validation to userSchema

--- src/shared/modules/user/user.model.ts
+++ src/shared/modules/user/user.model.ts
@@ -8,11 +8,24 @@ export interface UserDocument extends User, Document {
 
 const userSchema = new Schema(
   {
-    avatar: String,
-    email: { type: String, unique: true },
-    name: String,
-    password: String,
-    userType: String,
+    avatar: {type: String, unique: true, minlength: [5, 'Min length for avatar path is 5']},
+    email: {
+      type: String,
+      unique: true,
+      match: [/^([\w-\\.]+@([\w-]+\.)+[\w-]{2,4})?$/, 'Email is incorrect'],
+      require: true,
+    },
+    name: {
+      type: String,
+      require: true,
+      minlength: [2, 'Min length for name is 2'],
+    },
+    password: {
+      type: String,
+      require: true,
+      minlength: [8, 'Min length for password is 8'],
+    },
+    userType: {type: String, require: true}
   },
   { timestamps: true }
 );
