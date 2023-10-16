903cb64b34621b880ac9d42cdf2422001882e082
vpalex999 <vpalex@mail.ru>
2023-10-16 17:45:56 +0500

    inject Logger to DefaultUserService

--- src/shared/modules/user/default-user.service.ts
+++ src/shared/modules/user/default-user.service.ts
@@ -2,8 +2,14 @@ import { DocumentType } from '@typegoose/typegoose';
 import { CreateUserDto } from './index.js';
 import { UserService } from './user-service.interface.js';
 import { UserEntity, UserModel } from './user.entity.js';
+import { inject, injectable } from 'inversify';
+import { Component } from '../../types/index.js';
+import { Logger } from '../../libs/logger/index.js';
 
+@injectable()
 export class DefaultUserService implements UserService {
+  constructor(@inject(Component.Logger) private readonly logger: Logger) {}
+
   public async create(
     dto: CreateUserDto,
     salt: string
@@ -11,6 +17,9 @@ export class DefaultUserService implements UserService {
     const user = new UserEntity(dto);
     user.setPassword(dto.password, salt);
 
-    return UserModel.create(user);
+    const result = await UserModel.create(user);
+    this.logger.info(`New user created: ${user.email}`);
+
+    return result;
   }
 }
