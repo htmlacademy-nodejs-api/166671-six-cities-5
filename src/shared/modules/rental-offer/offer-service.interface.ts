5c12acbee03ea370e226d35e7a89d14a20a0fb4f
vpalex999 <vpalex@mail.ru>
2023-10-19 17:54:17 +0500

    add OfferService

---
+++ src/shared/modules/rental-offer/offer-service.interface.ts
@@ -0,0 +1,9 @@
+import { DocumentType } from '@typegoose/typegoose';
+import { CreateOfferDto } from './dto/create-offer.dto.js';
+import { RentalOfferEntity } from './rental-offer.entity.js';
+
+export interface OfferService {
+  create(dto: CreateOfferDto): Promise<DocumentType<RentalOfferEntity>>;
+  findByUserId(id: string): Promise<DocumentType<RentalOfferEntity | null>>;
+  findOrCreate(dto: CreateOfferDto): Promise<DocumentType<RentalOfferEntity>>;
+}
